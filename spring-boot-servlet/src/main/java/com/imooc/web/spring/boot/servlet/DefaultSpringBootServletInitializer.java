8fd2a457b150dcf48fb2b8666d7c4b29380a691b
wangyang <841234684@qq.com>
2019-04-28 21:28:46 +0800

    4/28

---
+++ spring-boot-servlet/src/main/java/com/imooc/web/spring/boot/servlet/DefaultSpringBootServletInitializer.java
@@ -0,0 +1,16 @@
+package com.imooc.web.spring.boot.servlet;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * {@link SpringBootServletInitializer}默认实现
+ */
+public class DefaultSpringBootServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(SpringBootServletBootStrap.class);
+  }
+}
--- spring-boot-servlet/src/main/java/com/imooc/web/spring/boot/servlet/SpringBootServletBootStrap.java
+++ spring-boot-servlet/src/main/java/com/imooc/web/spring/boot/servlet/SpringBootServletBootStrap.java
@@ -1,5 +1,64 @@
 package com.imooc.web.spring.boot.servlet;
 
+import com.imooc.web.spring.servlet.AsyncServlet;
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.FilterRegistration.Dynamic;
+import javax.servlet.Servlet;
+import javax.servlet.ServletRegistration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+/**
+ * spring boot 内置容器不支持ServletContainerInitializer ，不会自动加载WebApplicationInitializer--->AbstractAnnotationConfigDispatcherServletInitializer
+ * 解决方案：ServletContextInitializer
+ */
+@EnableAutoConfiguration
+//@ServletComponentScan(basePackages = "com.imooc.web.spring.servlet")//spring boot 不完全支持@WebServlet @WebFilter  @WebListener,需要依赖ServletComponentScan
+//@ComponentScan(basePackages = "com.imooc.web.spring.servlet.controller")
 public class SpringBootServletBootStrap {
 
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootServletBootStrap.class, args);
+  }
+
+  /**
+   * 也可以使用ServletRegistrationBean 注册servlet
+   *
+   *
+   * @return
+   */
+  @Bean
+  @Order(Ordered.HIGHEST_PRECEDENCE)
+  public ServletRegistrationBean asyncServletServletRegistrationBean() {
+    //ServletRegistrationBean默认支持异步
+    //    registrationBean.setAsyncSupported(true);
+    //相同的servlet 只会被注册一次，如果使用传统servlet容器，由于支持@webServlet 会先注册注解本身定义的servlet，这里再注册一次的话，就不能生效了
+    ServletRegistrationBean<Servlet> registrationBean = new ServletRegistrationBean<>(new AsyncServlet(), "/");
+    //所以需要重新设置一个servlet名称
+    registrationBean.setName("MyAsyncServlet");
+    return registrationBean;
+  }
+
+  @Bean
+  public ServletContextInitializer servletContextInitializer() {
+    return servletContext -> {
+      //或者使用ServletContextInitializer注册servlet filter
+     /* ServletRegistration.Dynamic dynamic = servletContext.addServlet("async-servlet", new AsyncServlet());
+      dynamic.addMapping("/async-servlet2");
+      dynamic.setAsyncSupported(true);*/
+      CharacterEncodingFilter filter = new CharacterEncodingFilter();
+      Dynamic registration = servletContext.addFilter("filter", filter);
+      registration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/");
+    };
+  }
 }
